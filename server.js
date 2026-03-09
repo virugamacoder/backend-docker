2081efa1654918df608d21754081479a81bfaa67
virugamacoder <virugama0@gmail.com>
2026-03-09 15:00:02 +0530

    Add Project Setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Node API running with Docker 🚀"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
